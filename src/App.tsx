3048c11d0d4cefe15052d359be334eda96152aa9
Abytova Saikal <saikalabytova1@gmail.com>
2023-05-24 21:53:02 +0600

    Created loop function to show all taskes from the state

--- src/App.tsx
+++ src/App.tsx
@@ -5,35 +5,58 @@ import Task from "./Task/Task";
 
 const App = () => {
   const[messages, setMessage] = useState([
-    {value: 'Add new task', id: '1234'}
+      {task: 'Add new task', id: '1234'},
+      {task: 'Buy coffee', id: '5678'},
+      {task: 'Exercise', id: '9112'},
   ]);
 
   const addTask = (e: React.ChangeEvent<HTMLInputElement>)=> {
     const messagesCopy = [...messages];
     const messageCopy = {...messagesCopy[0]};
-    messageCopy.value = e.target.value;
+    // messagesCopy.push(newMessage);
+    messageCopy.task = e.target.value;
     messagesCopy[0] = messageCopy;
-    // e.preventDefault();
     setMessage(messagesCopy);
-    // console.log(e.target.value);
+  };
+
+  const randomNum = () => {
+      const date = new Date();
+      return date;
   };
 
   const sendMessage = (event: React.MouseEvent<HTMLFormElement>) => {
     event.preventDefault();
-    const messagesCopy = [...messages];
-    console.log(messagesCopy);
+    const currentTask = messages[0].task;
+    const newtask = {
+        task: currentTask,
+        id: `${randomNum()}`,
+    };
+    messages.push(newtask);
+    console.log(messages);
   };
 
   const removeMessage = () => {
     console.log('lets imagine that message was removed');
-  }
+  };
+
+  const messageList = messages.map(oneMessage => {
+    return(
+      <Task
+          message={oneMessage.task}
+          onRemoveMessage={removeMessage}
+          key={oneMessage.id}
+      />
+    );
+  });
+
   return (
       <div className="App">
+          <h1>To-do list!</h1>
         <div>
-          <AddTaskFrom value={messages[0].value} onChangeMessage={addTask} onSubmitMessage={sendMessage}/>
+          <AddTaskFrom value={messages[0].task} onChangeMessage={addTask} onSubmitMessage={sendMessage}/>
         </div>
         <div>
-          <Task message="kakakak" onRemoveMessage={removeMessage}/>
+          {messageList}
         </div>
       </div>
   );
